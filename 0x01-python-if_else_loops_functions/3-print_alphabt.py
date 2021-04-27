ce56ff6b633e795168b252ee7ab89e37287eb126
ronLabs <ron.altamir@outlook.com>
2021-04-27 20:04:10 +0000

    prints the ASCII alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for alphab in range(97, 123):
-    if alphab == 101 or alphab == 113: #if alphab in [101, 103].. other way
+    # if alphab in [101, 103].. other way
+    if alphab == 101 or alphab == 113:
         continue
     print('{:c}'.format(alphab), end='')
