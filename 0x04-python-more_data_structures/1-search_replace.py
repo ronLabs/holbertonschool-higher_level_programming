8918f8cdc7d7c0ca473a0f5d4448bd22b83b50fc
ronLabs <ron.altamir@outlook.com>
2021-05-07 04:36:52 +0000

    function that replaces all occurrences of an element

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
 def search_replace(my_list, search, replace):
-    #lambda retorna replace si x=search sino el mismo x 
     my_list = list(map(lambda x: replace if x == search else x, my_list))
     return (my_list)
