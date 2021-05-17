5ae0ed18830edb6b8da028a9972900025de82c5f
ronLabs <ron.altamir@outlook.com>
2021-05-17 19:40:10 +0000

    Function prints elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for j in range(0, x):
+        try:
+            print(my_list[j], end='')
+            i = i + 1
+        except:
+            break
+    print()
+    return i
