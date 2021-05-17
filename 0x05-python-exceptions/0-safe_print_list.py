a5977032fc2b7fdaafd716d85ddfa43bff0b667b
ronLabs <ron.altamir@outlook.com>
2021-05-17 19:50:17 +0000

    Function prints elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[j], end='')
             i = i + 1
-        except:
+        except IndexError:
             break
     print()
     return i
