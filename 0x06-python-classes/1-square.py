167622ac9ced28113461c06c03a46e5aa1bba3c0
ronLabs <ron.altamir@outlook.com>
2021-05-20 23:49:56 +0000

    class Square that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,4 +5,4 @@
 class Square():
     """Square class with private instance attribute size"""
     def __init__(self, size):
-        self.__size = size  ## size is value of user input or program input
+        self.__size = size  # size is value of user input or program input
