26d4a3d143b0e715ad6b22bae2389b7335beb4ff
ronLabs <ron.altamir@outlook.com>
2021-05-25 04:32:16 +0000

    function that adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Create"""
+"""
+    Module for one function to get sum of two variables
+    can be either integers or floats, but floats must
+    be casted into integers.
+    Returns sums of the two integers.
+"""
 
 
 def add_integer(a, b=98):
-    """add two integer
-    Args:
-        a: int
-        b: int
-    """
-    if type(a) != int and type(a) != float:
+    """Method in which to get sum of two integers given
+       Checking if a or b is an int or float
+       Returning sum casted into an integer"""
+
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    else:
+        return int(a) + int(b)
