e31cd05c57b2ee074b17d0b2ab7618b1f25e46ef
ronLabs <ron.altamir@outlook.com>
2021-06-02 21:20:44 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module to expand BaseGeometry class"""
+
+
+class BaseGeometry():
+    """class BaseGeometry with method area"""
+    pass
+
+    def area(self):
+        """Pub Inst Method not implemented"""
+        raise Exception("area() is not implemented")
