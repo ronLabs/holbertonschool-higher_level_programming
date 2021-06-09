1d256c67bcaba5358b94a27105af69cb3f4abc28
ronLabs <ron.altamir@outlook.com>
2021-06-09 00:37:28 +0000

    Writing the class Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """get value of size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set value of size"""
+        self.__size = value
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """builtin to print string repr of instance"""
+        result = "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+                 self.id, self.x, self.y, self.size)
+        return result
