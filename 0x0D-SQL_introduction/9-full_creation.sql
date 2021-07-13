5b1e62c344533777989b5f74854d0e8aed300516
ronLabs <ron.altamir@outlook.com>
2021-07-13 04:30:21 +0000

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- creates the table "second_table"
+-- and adds multiple rows to table.
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUE (3, "Bob", 14);
+INSERT INTO `second_table`  (`id`, `name`, `score`) VALUE (4, "George", 8);
