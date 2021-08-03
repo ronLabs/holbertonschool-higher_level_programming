a73d3597f4a7b8c32eaf9fd38ddbc764887993f9
ronLabs <ron.altamir@outlook.com>
2021-08-03 16:36:47 +0000

    script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (num) {
-    if (!num || num < 2) {
-      return 1;
-    }
-    return num * factorial(num - 1);
+  if (!num || num < 2) {
+    return 1;
   }
-  
-  console.log(factorial(parseInt(process.argv[2])));
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(parseInt(process.argv[2])));
