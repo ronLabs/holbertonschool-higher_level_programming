685416820a9eaf5a8335a7121380f9801a7d6f93
ronLabs <ron.altamir@outlook.com>
2021-08-03 04:49:51 +0000

    script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
-if (isNaN(num)) {
+if (isNaN(num) === true) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < num; i++) {
