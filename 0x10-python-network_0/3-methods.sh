e5d0e879d1f6847152c94c8139fa820a0623c532
ronLabs <ron.altamir@outlook.com>
2022-02-12 18:38:50 +0000

    curl only methods

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI "$1" | grep Allow | cut -d " " -f1 --complement#!/bin/bash
+# script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI "$1" | grep Allow | cut -d " " -f1 --complement
