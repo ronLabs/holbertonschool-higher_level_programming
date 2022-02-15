e85f77c987e64b253a52fec532b84d8d9c317d62
ronLabs <ron.altamir@outlook.com>
2022-02-15 21:13:58 +0000

    POST an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" script that takes in a URL and an email address,
+    sends a POST request to the passed URL with
+    the email as a parameter, and finally displays
+    the body of the response. """
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    req_pag = argv[1]
+    send_email = {"email": argv[2]}
+    req = requests.post(req_pag, data=send_email)
+    print(req.text)
