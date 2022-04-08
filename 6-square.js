30e3ca654a5ec329f5acf005cf1d68e4e08a4b3c
ronLabs <ron.altamir@outlook.com>
2022-04-08 19:50:44 +0000

    Square#1

---
+++ 6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+module.exports = class Square extends Square1 {
+  charPrint (c) {
+    let char = 'X';
+    if (c !== undefined) {
+      char = c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.height));
+    }
+  }
+};
