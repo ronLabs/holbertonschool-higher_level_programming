80be3bf19e0eefd1cb65bf396450dab30de1faa7
ronLabs <ron.altamir@outlook.com>
2022-04-08 20:22:55 +0000

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const Square1 = require('./5-square.js');
-module.exports = class Square extend Square1 {
+module.exports = class Square extends Square1 {
   charPrint (c) {
     if (c === undefined) {
       this.print();
